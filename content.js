1219317ee9ee16adbc10ab21b3ce1ab5228b3257
Bipul Lamsal <bipullamsal@gmail.com>
2023-03-16 22:46:41 +0545

    Added Setopati Ads list

--- content.js
+++ content.js
@@ -1,7 +1,8 @@
 const onlineKhabar = ['okam-each-ad','okam-ad-position-wrap','ok_rb_addWrap','ok_roadblock','nh_widget_wrap','single-inbetween-stories'];
+const setoPati = ['main-bigyaapan','full-bigyaapan','bigyaapan-item','mobile-bigyaapan-only','desktop-bigyaapan-only'];
 
-
-const hide = document.querySelectorAll(onlineKhabar.map(className => '.' + className).join(', '));
+const allClassName = [...onlineKhabar, ...setoPati];
+const hide = document.querySelectorAll(allClassName.map(className => '.' + className).join(', '));
 
 for (let i = 0; i < hide.length; i++) {
     hide[i].remove();
